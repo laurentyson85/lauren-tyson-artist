58ade748c00d2c1067af9f4dec9bdafb2ad4036a
Lauren Tyson <102639873+laurentyson85@users.noreply.github.com>
2023-03-22 10:27:29 -0600

    Add test code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Lauren Tyson, Artist
         </p>
         <a
           className="App-link"
