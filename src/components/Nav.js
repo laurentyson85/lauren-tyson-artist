d0d6d2f43c4ae2475d47bff3a3b03fcdb641d3cf
Lauren Tyson <102639873+laurentyson85@users.noreply.github.com>
2023-04-01 12:17:16 -0600

    Replace link with navlink

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+
+let activeStyle = {
+  color:"white",
+  borderBottom: "2px solid white",
+}
 
 
 function Nav() {
   return (
     <div className="navBar">
         <nav>
-        <Link to="/">Home</Link>
-        <Link to="/hats">Hats</Link>
-        <Link to="/paintings">Paintings</Link>
+        <NavLink
+            to="/"
+            end
+            style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }
+          >
+            Home
+          </NavLink>        
+        <NavLink
+            to="hats"
+            end
+            style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }
+          >
+            All Jobs
+          </NavLink>       
+        <NavLink
+            to="paintings"
+            style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }
+          >
+            Hipster View
+          </NavLink>
         </nav>
     </div>
     
