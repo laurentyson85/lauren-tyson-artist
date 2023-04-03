1b09e69bceac9f30c9801cd597be5e8a52cdb850
Lauren Tyson <102639873+laurentyson85@users.noreply.github.com>
2023-04-03 11:53:17 -0600

    Make homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="footer">
       <p>Lauren Tyson, Artist <img id="footerImg" src={footerImg} alt="footerImg"/></p>
-      <p>Contact: laurentysonartist@gmail.com</p> 
+      <p>laurentysonartist@gmail.com</p> 
       <p>©2023 by Lauren Tyson. A registered Made in Montana business</p>      
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
-
+import { Link } from 'react-router-dom';
+import homeImg from "../images/lta_homepage_design.png"
+import ig from "../images/Lauren Tyson Artist Instagram.png"
 
 function Home() {
 
   return (
     <div className="home">
-      home
+      <img id="homeImg" src={homeImg} alt="homeImg"/>
+      <div className="homeText">
+        <p>Welcome! Use the navigation at the top to check out my work.</p>
+        <p>To contact me for orders, commissions, or anything else, please email 👉 laurentysonartist@gmail.com</p>
+        <p>Follow me on Instagram 👇 🥳 </p><Link to="https://www.instagram.com/lauren_tyson_artist/" target="_blank" rel="noreferrer"><img id="ig" src={ig} alt="ig"></img></Link>
+      </div>
     </div>
   );
 }
