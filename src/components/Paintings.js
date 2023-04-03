daa668d97ca3d68f4c2fe8bf2054afaa7899d461
Lauren Tyson <102639873+laurentyson85@users.noreply.github.com>
2023-04-03 13:04:21 -0600

    Add prints

--- src/components/Paintings.js
+++ src/components/Paintings.js
@@ -3,6 +3,141 @@ import React from "react";
 
 function Paintings() {
 
+  const prints = [
+    {
+      id: 1,
+      name: "On the Horizon",
+      type: "Art Print",
+      description: "16 x 20 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+      size: "16x20",
+      link: ,
+      price: "$35.00",
+      stock: "In stock"     
+    }, {
+      id: 1,
+      name: "This is Montana",
+      type: "Art Print",
+      description: "16 x 20 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+      size: "16x20",
+      link: ,
+      price: "$35.00",
+      stock: "In stock"          
+  }, {
+    id: 2,
+    name: "Indigenous",
+    type: "Art Print",
+    description: "10 x 10 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+    size: "10x10",
+    link: ,
+    price: "$20.00",
+    stock: "In stock"         
+  }, {
+    id: 3,
+    name: "On the River",
+    type: "Art Print",
+    description: "10 x 20 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+    size: "10x20",
+    link: ,
+    price: "$25.00",
+    stock: "In stock"         
+  }, {
+    id: 4,
+    name: "In the Wild",
+    type: "Art Print",
+    description: "10 x 20 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+    size: "10x20",
+    link: ,
+    price: "$25.00",
+    stock: "In stock"           
+  }, {
+    id: 5,
+    name: "Sunset Trout",
+    type: "Art Print",
+    description: "10 x 20 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+    size: "10x20",
+    link: ,
+    price: "$25.00",
+    stock: "In stock"           
+  }, {
+    id: 6,
+    name: "Trout Skin",
+    type: "Art Print",
+    description: "10 x 20 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+    size: "10x20",
+    link: ,
+    price: "$25.00",
+    stock: "In stock"           
+  }, {
+    id: 7,
+    name: "Rainbow",
+    type: "Art Print",
+    description: "10 x 20 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+    size: "10x20",
+    link: ,
+    price: "$25.00",
+    stock: "In stock"           
+  }, {
+    id: 8,
+    name: "War Dance",
+    type: "Art Print",
+    description: "10 x 20 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+    size: "10x20",
+    link: ,
+    price: "$25.00",
+    stock: "In stock"          
+  }, {
+    id: 9,
+    name: "Water & Wind",
+    type: "Art Print",
+    description: "10 x 10 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+    size: "10x10",
+    link: ,
+    price: "$20.00",
+    stock: "In stock"           
+  }, {
+    id: 10,
+    name: "Bald Eagle, Grey Day",
+    type: "Art Print",
+    description: "16 x 20 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+    size: "16x20",
+    link: ,
+    price: "$35.00",
+    stock: "In stock"           
+  }, {
+    id: 11,
+    name: "Trout Montana",
+    type: "Art Print",
+    description: "11 x 14 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+    size: "11x14",
+    link: ,
+    price: "$25.00",
+    stock: "In stock"           
+  }, {
+    id: 12,
+    name: "Riderless Horse",
+    type: "Art Print",
+    description: "10 x 10 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+    size: "10x10",
+    link: ,
+    price: "$20.00",
+    stock: "In stock"           
+  },
+  {
+    id: 13,
+    name: "On the Horizon",
+    type: "Art Print",
+    description: "16 x 20 fine art print on smooth matte paper made from 100% cotton hot press paper.",
+    size: "16x20",
+    link: ,
+    price: "$35.00",
+    stock: "In stock"           
+  }
+]
+
+  const originals = [
+    
+  ]
+
   return (
     <div className="paintings">
       Paintings
